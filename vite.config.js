5be6368aec904ffd8410ee7c9d7dc4c1b0c4e361
backendINFJ <backendINFJ@gmail.com>
2024-07-31 18:36:08 +0900

    feature : 회원가입 기능구현

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+ import { defineConfig } from 'vite'
+ import vue from '@vitejs/plugin-vue'
 
-function fileURLToPath(url) {
-  return "";
-}
+ function fileURLToPath(url) {
+   return "";
+ }
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/users/signup': {
-        target: 'http://localhost:8080', // 실제 API 서버 주소로 변경하세요.
-        changeOrigin: true
-      }
-    }
-  }
-})
+ export default defineConfig({
+   plugins: [vue()],
+   resolve: {
+     alias: {
+       '@': fileURLToPath(new URL('./src', import.meta.url))
+     }
+   },
+   server: {
+     proxy: {
+       '/users/signup': {
+         target: 'http://localhost:8080', // 실제 API 서버 주소로 변경하세요.
+         changeOrigin: true
+       }
+     }
+   }
+ })
