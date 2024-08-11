640fb41cc4b23b5cd9c5f2890db47bdc7415fe9e
semdev <tpal071997@gmail.com>
2024-08-11 17:22:47 +0900

    fix : #15 remove commonjs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import commonjs from '@rollup/plugin-commonjs'
 
 
 export default defineConfig({
-  plugins: [vue(), commonjs()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
